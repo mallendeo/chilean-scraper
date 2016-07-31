4857da4de54c2f91d1c20c597a3df28bc155abc2
Mauricio Allende <mauricio.allende.o@gmail.com>
2016-07-30 21:50:49 -0400

    update ripley scraper with new module structure

--- src/lib/scrapers/ripley.js
+++ src/lib/scrapers/ripley.js
@@ -5,30 +5,26 @@ import cheerio from 'cheerio'
 const HOST = 'http://simple.ripley.cl'
 const SEARCH_URL = `${HOST}/search/`
 
-const ripleyScraper = () => {
-  const getProducts = (page = 0, qty = 10, search = '*') => {
-    const url = `${SEARCH_URL}${search}?&page=${page}&pageSize=${qty}`
-    return getBody(url).then(body => {
-      const $ = cheerio.load(body)
-      const elems = $('.catalog-container .catalog-product')
+// TODO: get navigation
 
-      return elems.map((i, elem) => {
-        const link  = $(elem).find('a.catalog-item').attr('href')
-        const img   = $(elem).find('.product-image img').attr('data-src')
-        const name  = $(elem).find('.catalog-product-name').text()
-        const price = $(elem).find('.best-price').text().replace(/([^\d])/ig, '')
+export const getProducts = (page = 0, qty = 10, search = '*') => {
+  const url = `${SEARCH_URL}${search}?&page=${page}&pageSize=${qty}`
+  return getBody(url).then(body => {
+    const $ = cheerio.load(body)
+    const elems = $('.catalog-container .catalog-product')
 
-        return {
-          name: cleanText(name),
-          price: parseInt(price.replace(/[\$\.]/g, '')),
-          link: `${HOST}${link}`,
-          image: img,
-        }
-      }).get()
-    })
-  }
+    return elems.map((i, elem) => {
+      const link = $(elem).find('a.catalog-item').attr('href')
+      const img = $(elem).find('.product-image img').attr('data-src')
+      const name = $(elem).find('.catalog-product-name').text()
+      const price = $(elem).find('.best-price').text().replace(/([^\d])/ig, '')
 
-  return { getProducts }
+      return {
+        name: cleanText(name),
+        price: parseInt(price.replace(/[\$\.]/g, '')),
+        link: `${HOST}${link}`,
+        image: img,
+      }
+    }).get()
+  })
 }
-
-export default ripleyScraper
