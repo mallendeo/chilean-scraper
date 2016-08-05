7f9e01378660ad5d95f10e27f7f513397ed58d67
Mauricio Allende <mauricio.allende.o@gmail.com>
2016-08-05 16:16:33 -0400

    add paris test; fix nav

--- src/lib/scrapers/paris.js
+++ src/lib/scrapers/paris.js
@@ -3,26 +3,35 @@ import { cleanText, getDOM } from '../helpers'
 export const HOST = 'http://www.paris.cl'
 const SEARCH_URL = `${HOST}/webapp/wcs/stores/servlet/AjaxCatalogSearchResultView`
 
-export const makeUrl = (page = 0, qty = 10, search = '') => `${SEARCH_URL}?storeId=10801`
-  + `&pageSize=${qty}&beginIndex=${page * qty}&sType=SimpleSearch&searchTerm=${search}`
+export const makeUrl = (page = 1, qty = 10, search = '') => `${SEARCH_URL}?storeId=10801`
+  + `&pageSize=${qty}&beginIndex=${(page - 1) * qty}&sType=SimpleSearch&searchTerm=${search}`
 
-export const getNav = ($, qty, search) => {
+export const getNav = ($, res) => {
   const nav = $('.paging ul').first()
   const current = nav.find('.selected')
-  const prev = cleanText(current.prev().text())
-  const next = cleanText(current.next().text())
+
+  const checkElem = elem => {
+    if (elem.attr('style')) return null
+    const match = $(elem).children('a')
+      .attr('onclick')
+      .match(/goToResultPage\('(.+?)',/)
+    return match ? match[1] : null
+  }
+
+  const prev = checkElem(current.prev())
+  const next = checkElem(current.next())
 
   return {
-    prev: prev ? makeUrl(prev, qty, search) : null,
-    current: makeUrl(cleanText(current.text()), qty, search),
-    next: next ? makeUrl(next, qty, search) : null,
+    prev: prev || null,
+    current: HOST + res.req.path,
+    next: next || null,
   }
 }
 
-export const parseProducts = $ => {
+export const parseProducts = ($, res) => {
   const elems = $('.item_container > .item')
 
-  const nav = getNav($)
+  const nav = getNav($, res)
   const products = elems.map((i, elem) => {
     const link = $(elem).find('.description_fixedwidth > a').attr('href')
     const img = $(elem).find('.img img').attr('src')
@@ -41,4 +50,4 @@ export const parseProducts = $ => {
 }
 
 export const getProducts = (page, qty, search) =>
-  getDOM(makeUrl(page, qty, search)).then(({ body }) => parseProducts(body))
+  getDOM(makeUrl(page, qty, search)).then(({ $, res }) => parseProducts($, res))
---
+++ test/paris.js
@@ -0,0 +1,32 @@
+import test from 'ava'
+import { matchProducts } from './helpers'
+
+import { parseProducts, getProducts } from '../src/lib/scrapers/paris'
+
+import data from './data/paris.json'
+import pages from './pages/paris.json'
+
+test('Parse the pages and match the list of products', t =>
+  t.true(matchProducts(pages, data, parseProducts)))
+
+test('[live] Get the first page and return a list of 10 products', async t => {
+  try {
+    t.true((await getProducts(1)).products.length === 10)
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('[live] Get the second page and return a list of 20 products', async t => {
+  const pageSize = 10
+  const page = 4
+  try {
+    const { products, nav } = await getProducts(page, pageSize)
+    t.true(products.length === pageSize
+      && nav.prev.includes(`beginIndex=${pageSize * (page - 2)}`)
+      && nav.current.includes(`beginIndex=${pageSize * (page - 1)}`)
+      && nav.next.includes(`beginIndex=${pageSize * page}`))
+  } catch (e) {
+    t.fail(e)
+  }
+})
