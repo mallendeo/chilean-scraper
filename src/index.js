d42bf7c867e0d4afdf247edea44fa65020446561
Mauricio Allende <mauricio.allende.o@gmail.com>
2016-08-18 14:00:11 -0300

    move handler outside and make it the main file

--- src/index.js
+++ src/index.js
@@ -1,40 +1,91 @@
-/* eslint no-console:0 */
+import EventEmitter from 'events'
+import scrapers from './lib/scrapers'
 
-import chalk from 'chalk'
-import handler from './lib/handler'
+const loadScraper = scraperId => {
+  if (!scraperId || !scrapers[scraperId]) throw new Error('Invalid scraper ID')
 
-const scraper = handler.loadScraper('paris')
+  const scraper = scrapers[scraperId]
+  const emitter = new EventEmitter()
 
-scraper.getAllProducts()
+  const getAllProducts = (opts = {}) => {
+    opts.page = opts.page || 1
+    opts.cIndex = opts.cIndex || 0
+    opts.i = opts.i || 0
 
-scraper.emitter.on('gettingProducts', data => {
-  console.log(chalk.green('\n\t============================='))
+    if (opts.total && parseInt(opts.i, 10) === parseInt(opts.total, 10)) {
+      return emitter.emit('done', { pages: opts.total })
+    }
 
-  if (!data.cList) {
-    console.log(chalk.white.bold('page:'), data.page)
+    if (scraper.getCategories) {
+      // Get products by categories
+      if (!opts.cList) {
+        setImmediate(() => emitter.emit('gettingCategories'))
+
+        return scraper.getCategories().then(categories => {
+          emitter.emit('gotCategories', categories)
+          opts.cList = categories
+          return getAllProducts(opts)
+        })
+      }
+
+      const category = opts.cList[opts.cIndex]
+      const nextCategory = opts.cList[opts.cIndex + 1]
+
+      emitter.emit('gettingProducts', opts)
+
+      return scraper.getProducts(category.href, opts.page)
+        .then(data => {
+          data.category = category
+          emitter.emit('gotProducts', { data, opts })
+
+          if (data.nav.next) {
+            opts.page += 1
+            opts.i += 1
+            return getAllProducts(opts)
+          }
+
+          if (nextCategory) {
+            opts.page = 1
+            opts.cIndex += 1
+            opts.i += 1
+            return getAllProducts(opts)
+          }
+
+          return emitter.emit('done')
+        })
+        .catch(err => {
+          emitter.emit('error', err)
+          return getAllProducts(opts)
+        })
+    }
+
+    // Get products vía search url
+    setImmediate(() => emitter.emit('gettingProducts', opts))
+
+    return scraper.getProducts(opts.page, opts.qty)
+      .then(data => {
+        emitter.emit('gotProducts', { data, opts })
+
+        if (data.nav.next) {
+          opts.page += 1
+          opts.i += 1
+          return getAllProducts(opts)
+        }
+        return emitter.emit('done')
+      })
+      .catch(err => {
+        emitter.emit('error', err)
+        return getAllProducts(opts)
+      })
   }
 
-  if (data.total) {
-    console.log(chalk.white.bold('\tprogress:'),
-      chalk.cyan(data.i + 1), 'of', chalk.cyan(data.total), '\n')
+  return {
+    getAllProducts,
+    emitter,
   }
+}
 
-  if (data.cList) {
-    const category = data.cList[data.cIndex]
-    const nextCategory = data.cList[data.cIndex + 1]
-    console.log(chalk.white.bold('\tname:'), chalk.cyan(category.name))
-    console.log(chalk.white.bold('\tcategory:'),
-      chalk.cyan(data.cIndex), 'of', chalk.cyan(data.cList.length))
-    console.log(chalk.white.bold('\tnext category:'), chalk.cyan(nextCategory.name))
-  }
-
-  console.log(chalk.green('\t============================='))
-  console.log(chalk.red('-----------------------------\n'))
-})
-
-let total = 0
-scraper.emitter.on('gotProducts', ({ data: { products, nav } }) => {
-  console.log(chalk.cyan('  # products total:'), chalk.green(total += products.length))
-  console.log(chalk.cyan('  # nav:'), chalk.green('prev'),
-    nav.prev, '|', chalk.green('next:'), nav.next)
-})
+export default {
+  scrapers,
+  loadScraper,
+}
